3d3d7429bed1f3e6ddccf4515e1c31dfedae867e
babs20 <thevillemc@gmail.com>
2021-04-13 14:58:51 -0400

    rename file

---
+++ content.js
@@ -0,0 +1,26 @@
+// document.querySelector('.k1zIA').classList.add('spinspinspin');
+
+const first = document.createElement('button');
+first.innerText = 'SET DATA';
+first.id = 'first';
+
+const second = document.createElement('button');
+second.innerText = 'SHOUT TO BACKEND';
+second.id = 'second';
+
+document.querySelector('body').appendChild(first);
+document.querySelector('body').appendChild(second);
+
+first.addEventListener('click', () => {
+  chrome.storage.local.set({ password: '123' });
+  console.log('hit it');
+});
+
+second.addEventListener('click', () => {
+  chrome.runtime.sendMessage({ message: 'check the storage' });
+  console.log('sent it');
+});
+
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+  console.log(req.message);
+});
