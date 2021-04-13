7865e744376e52720ea21c924ad35f93e4e7b8b2
babs20 <thevillemc@gmail.com>
2021-04-13 14:49:21 -0400

    add state and message handling

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
+let activeTabId = 0;
+
 chrome.tabs.onActivated.addListener(tab => {
   chrome.tabs.get(tab.tabId, currentTabInfo => {
+    activeTabId = tab.tabId;
     if (/^https:\/\/www\.google/.test(currentTabInfo.url)) {
       chrome.tabs.insertCSS(null, { file: './mystyles.css' });
       chrome.tabs.executeScript(null, { file: './foreground.js' }, () => {
@@ -7,4 +10,14 @@ chrome.tabs.onActivated.addListener(tab => {
       });
     }
   });
+});
+
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+  if (req.message === 'check the storage') {
+    chrome.tabs.sendMessage(activeTabId, {
+      message: 'i got the message - background',
+    });
+
+    chrome.storage.local.get('password', value => console.log(value));
+  }
 });
